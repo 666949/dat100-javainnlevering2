3cebbbd1a7dbf500741163a37ac193f41938f83f
Sondre L. Ness <111089396+666949@users.noreply.github.com>
2022-09-28 11:39:33 +0200

    jj

---
+++ src/no/hvl/dat100/lab5/tabeller/Main.java
@@ -0,0 +1,18 @@
+package no.hvl.dat100.lab5.tabeller;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		// tilStreng
+//		int[] tabell = {1,2,3,4,5,6,7};
+//		String tab = Tabeller.tilStreng(tabell);
+//		System.out.println(tab);
+//		
+		
+		
+
+	}
+	
+	
+}
--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -1,79 +1,132 @@
 package no.hvl.dat100.lab5.tabeller;
 
 public class Tabeller {		
-
-public static void main(String[] args) {
-	
-	int[] tabell = {1,2,3,42,67,89,3,2,1};
-	
-	skrivUt(tabell);
-	
-	tilStreng(tabell); 
-
-}
 	// a) 
 	public static void skrivUt(int[] tabell) {
 
-
-		System.out.print("[");
-		for(int i = 0; i<tabell.length;i++){
-			System.out.print(tabell[i] + ", ");
+		int[] tab = {1,2,3,4,5};
+		
+		System.out.print("[ ");
+		for(int i = 0; i<tab.length;i++){
+			System.out.print(tab[i] + ", ");
 		}
 		System.out.print("]");
 
 		}
-
-
 	// b)
 	public static String tilStreng(int[] tabell) {
 		
-		String tab = "[42, 67, 89]";
-		System.out.println();
-		System.out.println(tab);
-		return tab;
-	}
-
-	// c)
-	public static int summer(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
-	}
-
-	// d)
-	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("finnesTall ikke implementert");
-	}
-
-	// e)
-	public static int posisjonTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
-
-	}
-
-	// f)
-	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("reverser ikke implementert");
+		String skilletegn;
+		String tabellStreng = "["; 
+		
+		for(int i = 0; i < tabell.length; i++) {
+			if (i == (tabell.length - 1)) {
+				skilletegn = "";
+			} else {
+				skilletegn = ",";
+			}
+			
+		tabellStreng += tabell[i];
+		tabellStreng += skilletegn; 
+		}
+		
+		tabellStreng += "]";
+		
+		return tabellStreng;
+			
 		
 	}
-
+	// c)
+	public static int summer(int[] tabell) {
+		
+		int sum = 0;
+		
+		for(int i : tabell)
+			sum += i;
+		
+//		for(int i = 0; i<tabell.length; i++) {
+//			sum += tabell[i]; 
+//		}
+		
+//		int i = 0; 
+//		while (i<tabell.length) {
+//			sum += tabell[i];
+//			i++;
+			
+//		}
+		
+		return sum;
+	}
+	// d)
+	public static boolean finnesTall(int[] tabell, int tall) {
+		
+				
+		int i = 0; 
+		boolean finnes = false; 
+		
+		while (i < tabell.length && !finnes) {
+			if (tabell[i] == tall) {
+				finnes = true;
+			} else {
+				i++;
+			}
+		}
+		
+		return finnes; 
+	}
+	// e)
+	public static int posisjonTall(int[] tabell, int tall) {
+		int minstePos = 0;
+		
+		for (int i = 0; i < tabell.length; i++) {
+			if (tabell[i] < tabell[minstePos]) {
+				minstePos = i;
+				} else {
+					return -1; 
+				}
+			}
+		return minstePos; 
+	}
+	// f)
+	public static int[] reverser(int[] tabell) {
+		
+		if(tabell == null || tabell.length <= 1){
+            System.out.println("Ugyldig tabell.");
+        }
+        for (int i = 0; i < tabell.length / 2; i++) {
+            int temp = tabell[i];
+            tabell[i] = tabell[tabell.length - 1 - i];
+            tabell[tabell.length - 1 - i] = temp;
+        }
+        return tabell;
+	}
 	// g)
 	public static boolean erSortert(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("erSortert ikke implementert");
+		for (int i = 0; i < tabell.length-1; i++) {
+	        if (tabell[i] > tabell[i + 1]) {
+	            return false;
+	        }
+	    }
+	    return true;
 	}
-
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
 
-		// TODO
-		throw new UnsupportedOperationException("settSammen ikke implementert");
+		int[] tabell3 = new int[tabell1.length+tabell2.length];
+		int c = 0; 
+		
+		for(int i = 0; i < tabell1.length; i++) {
+			tabell3[i] = tabell1[i];
+			c++;
+		}
+		for(int j = 0; j < tabell2.length; j++) {
+			tabell3[c++] = tabell2[j];
+		}
+		for(int i=0; i<tabell3.length;i++) {
+			System.out.println(tabell3[i]+" ");
+		}
+		
+		return tabell3; 
 	}
 }
